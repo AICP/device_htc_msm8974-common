b3b9e56b667e72e98927c4883f2e9145152ce5d9
Julian Veit <claymore1298@gmail.com>
2019-01-06 09:27:44 +0100

    Revert "msm8974-common: Use QTI HIDL power HAL"
    This reverts commit c7bcd3275049bb8e6f9801311e6d02d8f8290fa1.

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -245,7 +245,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl \
+    power.msm8974
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
