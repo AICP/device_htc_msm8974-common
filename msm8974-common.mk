c70bec46cbee54de9a7d3f706410d145ae6d59a6
Julian Veit <claymore1298@gmail.com>
2019-01-10 21:01:34 +0100

    msm8974-common: build SnapdragonCamera

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     libshim_camera \
-    Snap
+    SnapdragonCamera
 
 # DRM
 PRODUCT_PACKAGES += \
