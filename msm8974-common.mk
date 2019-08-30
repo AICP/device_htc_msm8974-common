09f20e16ff5d5d7e4f3ffad8f50238567b7e2258
Julian Veit <claymore1298@gmail.com>
2019-08-30 19:35:25 +0200

    msm8974-common: add telephony-ext
    We need that in Q anyway, so add it
    
    Change-Id: Ie7ae348f2a58f1ab6dd6583b00d2a2fc39dae02a

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -273,6 +273,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# codeaurora telephony-ext
+PRODUCT_PACKAGES += \
+    telephony-ext
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Thermal
 PRODUCT_PACKAGES += \
     thermanager
