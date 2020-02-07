afa6aa4d1be2a08ec302f4d66645d30589b8cc7d
Julian Veit <claymore1298@gmail.com>
2020-02-07 15:45:15 +0100

    msm8974-common: switch back to good ol' power HAL
    This reverts commit a89e2ac3e6a9a39801816cb62598ba8ed8f85453.
    
    Change-Id: I42df03ff3185da3c62a2cff0ea1a3406fcb41c95

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -248,7 +248,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.0-impl \
+    power.msm8974
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
