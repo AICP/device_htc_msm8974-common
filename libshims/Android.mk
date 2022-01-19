f3c1e09a9bf85baf893d18f3e94455a70bfa56c3
TARKZiM <tom8476oo@gmail.com>
2022-01-20 00:43:21 +0800

    msm8974-common: libshims: Merge the camera shims

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,8 +16,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := camera_shim.cpp
-LOCAL_SHARED_LIBRARIES := libui
+LOCAL_SRC_FILES := camera_shim.cpp sensor.cpp
+LOCAL_SHARED_LIBRARIES := libui libsensor libutils
 LOCAL_MODULE := libshim_camera
 include $(BUILD_SHARED_LIBRARY)
 
