6ab1fe14483a12b4c52ee0f76368b983312ecace
Julian Veit <claymore1298@gmail.com>
2022-08-19 18:29:30 +0200

    msm8974-common: remove com.dsi.ant.antradio_library.xml
    Change-Id: I566b2abb91db3d0a3cf436e2621623c3ebace52a

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -67,7 +67,6 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
